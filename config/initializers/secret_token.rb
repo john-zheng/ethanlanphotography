fc9af5f55a5457313f63d892a48219f06b0b85ca
John Zheng <zheng8001@gmail.com>
2013-06-06 20:19:20 +0930

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ethanlanphotography::Application.config.secret_token = '0023a5062fd8f0e323708328a706906b158a4a146ca374a7b4b4f5d88cbbb04cc94f63f68ba452e171934f687ee105e6694b5929fe08029faf01400cda7e86af'
