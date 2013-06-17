32577f9634b2aed36d2241575bec91581527e8be
John Zheng <zheng8001@gmail.com>
2013-06-17 23:43:04 +0930

    new pages

--- app/controllers/cn_controller.rb
+++ app/controllers/cn_controller.rb
@@ -4,20 +4,26 @@ class CnController < ApplicationController
   end
 
   def work
+    @title = "Work"
   end
 
   def photo
+    @title = "Photo"
   end
 
   def video
+    @title = "Video"
   end
 
   def price
+    @title = "Price"
   end
 
   def about
+    @title = "About"
   end
 
   def contact
+    @title = "Contact"
   end
 end
